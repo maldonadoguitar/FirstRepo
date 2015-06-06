93dd05e5a1c60d44aeb9d4d1b851116744f5e29d
Lauren <laurencaponong@gmail.com>
2015-06-06 11:02:44 -0400

    first commit

---
+++ arrays/arrays/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  arrays
+//
+//  Created by Lauren Caponong on 6/6/15.
+//  Copyright (c) 2015 Lauren Caponong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
